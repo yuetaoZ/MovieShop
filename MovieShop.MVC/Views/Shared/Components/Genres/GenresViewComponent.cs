1257bc494fe4eae0051a12e0c0ca5a972242a237
YuetaoZ <yuetao.zhu.062984@gmail.com>
2021-06-10 15:38:30 -0500

    Added components for Genre drop down list.

---
+++ MovieShop.MVC/Views/Shared/Components/Genres/GenresViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationCore.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MovieShop.MVC.Views.Shared.Components.Genres
+{
+    public class GenresViewComponent: ViewComponent
+    {
+        private readonly IGenreService _genreService;
+
+        public GenresViewComponent(IGenreService genreService)
+        {
+            _genreService = genreService; 
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var genres = await _genreService.GetAllGenres();
+            return View(genres);
+        }
+    }
+}
