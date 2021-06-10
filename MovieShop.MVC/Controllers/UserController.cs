9499a880c384fda4d6ea204187a623443a0709e6
YuetaoZ <yuetao.zhu.062984@gmail.com>
2021-06-10 14:17:57 -0500

    Updated Login View Display for show Logined status.

--- MovieShop.MVC/Controllers/UserController.cs
+++ MovieShop.MVC/Controllers/UserController.cs
@@ -21,18 +21,23 @@ namespace MovieShop.MVC.Controllers
         [HttpGet]
         public async Task<IActionResult> GetUserPurchasedMovies()
         {
+
             var userId = _currentUserService.UserId;
-            
+            // get the user id
+            //
+            // make a request to the database and get info from purchase table 
+            // select * from Purchase where userid = @getfromcookie
             return View();
         }
 
+
+        [Authorize]
         [HttpPost]
         public async Task<IActionResult> PurchaseMovie()
         {
-            // get userid from CurrentUswer and create a row in Purchased Table
+            // get userid from CurrentUser and create a row in Purchase Table
             return View();
         }
-
-        // ViewProfile
     }
 }
+
